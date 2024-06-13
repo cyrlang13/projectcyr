29ebf4f6c48cbab965d879a0ad4bd078c736dffc
cyrlang13 <cyrlang13@naver.com>
2024-06-13 12:15:36 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let x = 0;
+let y = 0;
+let easing = 0.01;
+
+function setup() {
+createCanvas(1280, 800);
+
+}
+
+function draw(){
+    let targetX = mouseX;
+    let dx = targetX - x;
+    x += dx * easing;
+    let targetY = mouseY;
+    let dy = targetY - y;
+    y += dy * easing;
+
+    let d = dist(mouseX, mouseY, pmouseX, pmouseY);
+   d = map(d, 0, 600, 20, 200);
+    ellipse(x, height/2, 100);
+}
