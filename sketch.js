ced31e7ad8d27183f8927b536821c9afa126cb5c
cyrlang13 <cyrlang13@naver.com>
2024-06-20 12:54:32 +0900

    2
    add mountain

--- sketch.js
+++ sketch.js
@@ -2,7 +2,16 @@ let dino;
 let obstacles = [];
 let farClouds = [];
 let closeClouds = [];
+let mountain = [];
+let bush = [];
 let score = 0;
+let imgHuman, imgObstacle;
+
+
+function preload() {
+  imgHuman = loadImage('img/human.jpg');
+  imgObstacle = loadImage('img/169958141.png');
+}
 
 function setup() {
   createCanvas(1000, 500);
@@ -12,25 +21,28 @@ function setup() {
 function draw() {
   background(0, 0, 100);
 
-  
   textSize(30);
   text('Score: ' + score, 10, 30);
-
   
-  dino.update();
-  dino.show();
 
- 
+  if (frameCount % 150 === 0) {
+    closeClouds.push(new Bush());
+  }
+  dino.update();
+
   if (frameCount % 100 === 0) {
     farClouds.push(new FarCloud());
   }
 
- 
+
   if (frameCount % 150 === 0) {
     closeClouds.push(new CloseCloud());
   }
 
-  
+  if (frameCount % 150 === 0) {
+    closeClouds.push(new Mountain());
+  }
+
   for (let i = farClouds.length - 1; i >= 0; i--) {
     farClouds[i].update();
     farClouds[i].show();
@@ -40,7 +52,7 @@ function draw() {
     }
   }
 
- 
+
   for (let i = closeClouds.length - 1; i >= 0; i--) {
     closeClouds[i].update();
     closeClouds[i].show();
@@ -50,12 +62,14 @@ function draw() {
     }
   }
 
-  
+  dino.show();
+
+
   if (frameCount % 60 === 0) {
     obstacles.push(new Obstacle());
   }
 
-  
+
   for (let i = obstacles.length - 1; i >= 0; i--) {
     obstacles[i].update();
     obstacles[i].show();
@@ -78,9 +92,12 @@ function keyPressed() {
 }
 
 function gameOver() {
+  textSize(100);
+  textAlign(CENTER, CENTER);
+  // fill(); 게임오버 텍스트 컬러 바꾸기
+  text("game Over", width/2, height/2);
   noLoop();
   console.log('Game Over');
-  
 }
 
 class Dino {
@@ -88,7 +105,7 @@ class Dino {
     this.x = 50;
     this.y = height - 50;
     this.velocityY = 0;
-    this.gravity = 0.5;
+    this.gravity = 0.5; //얼마나 높이 뛸수있는지
     this.isJumping = false;
   }
 
@@ -112,7 +129,16 @@ class Dino {
 
   show() {
     fill(0, 255, 0);
-    rect(this.x, this.y, 50, 50);
+    //여기 캐릭터 이미지
+    image(imgHuman, this.x, this.y - 45, 50, 90);
+    // rect(this.x, this.y, 50, 50);
+
+    if(this.isJumping){
+      image(imgHuman, this.x, this.y - 45, 50, 90);
+      //점프할때 또다른 이미지 소스 넣어주기
+    } else {
+      image(imgHuman, this.x, this.y - 45, 50, 90);
+    }
   }
 }
 
@@ -131,7 +157,9 @@ class Obstacle {
 
   show() {
     fill(255, 0, 0);
-    rect(this.x, this.y, this.width, this.height);
+    //여기 장애물 이미지
+    image(imgObstacle, this.x, this.y, 30, 30);
+    // rect(this.x, this.y, this.width, this.height);
   }
 
   offscreen() {
@@ -160,8 +188,9 @@ class FarCloud {
     this.x -= this.speed;
   }
 
-  show() {
+  show() { //far cloud 
     fill(200);
+
     rect(this.x, this.y, this.width, this.height);
   }
 
@@ -176,15 +205,16 @@ class CloseCloud {
     this.y = random(20, 130);
     this.width = 40;
     this.height = 20;
-    this.speed = 7;
+    this.speed = 10;
   }
 
   update() {
     this.x -= this.speed;
   }
 
-  show() {
+  show() { //close cloud
     fill(150);
+    //2:1비율 구름 image를 넣으세요
     rect(this.x, this.y, this.width, this.height);
   }
 
@@ -192,3 +222,51 @@ class CloseCloud {
     return this.x + this.width < 0;
   }
 }
+
+class Mountain {
+     constructor() {
+        this.x = width;
+        this.y = random(500, 130);
+        this.width = 1500;
+        this.height = 1000;
+        this.speed = 1;
+      }
+    
+      update() {
+        this.x -= this.speed;
+      }
+    
+      show() {
+        fill(0);
+        
+        rect(this.x, this.y, this.width, this.height);
+      }
+    
+      offscreen() {
+        return this.x + this.width < 0;
+      }
+}
+
+// class Bush {
+//     constructor() {
+//         this.x = width;
+//         this.y = random(1000, 10);
+//         this.width = 1500;
+//         this.height = 50;
+//         this.speed = 7;
+//       }
+    
+//       update() {
+//         this.x -= this.speed;
+//       }
+    
+//       show() {
+//         fill(50, 200, 70);
+        
+//         rect(this.x, this.y, this.width, this.height);
+//       }
+    
+//       offscreen() {
+//         return this.x + this.width < 0;
+//       }
+// }
